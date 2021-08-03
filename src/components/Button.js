5102c7c3e477b00fba147d4e4dd638e2d2358b0b
Tanvir Siddiqui <tanvir.artisan@gmail.com>
2021-08-04 01:00:41 +0600

    Header and Button components

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+const Button = ({ btnTxt, btnClr }) => {
+    return (
+        <>
+            <button className="btn" style={{backgroundColor:btnClr}}> {btnTxt} </button>
+        </>
+    )
+}
+
+export default Button;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import Button from "./Button"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Task Tracker</h1>
+            <Button btnTxt="Add" btnClr="Green" />
+        </div>
+    )
+}
+
+export default Header;
